b9ccf587a775a5e5ce1b6a3682b55de707c55fed
Pablo <Pablo>
2016-09-22 15:57:15 -0300

    php conection to SQL database

--- example.php
+++ example.php
@@ -1,8 +1,12 @@
-<html>
- <head>
-  <title>Prueba de PHP</title>
- </head>
- <body>
- <?php echo '<p>Hola Mundo</p>'; ?>
- </body>
-</html>
+<?php
+$serverName = "globeando.database.windows.net"; //serverName\instanceName
+$connectionInfo = array( "Database"=>"globeando", "UID"=>"Pablo", "PWD"=>"Pablito175");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+if( $conn ) {
+     echo "Conexión establecida.<br />";
+}else{
+     echo "Conexión no se pudo establecer.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
+?>
